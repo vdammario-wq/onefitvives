3c697bc25d25b11b3a7c8dd9aabbee5a452a7475
vdammario-wq <vdam.mario@gmail.com>
2026-08-23 21:17:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='onefitVIVES-v24';
-const STATISCH=['./','./index.html','./manifest.json','./manifest-rood.json','./icon-192.png','./icon-512.png','./icon-192-rood.png','./icon-512-rood.png'];
+const CACHE='onefitVIVES-v26';
+const STATISCH=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATISCH)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
